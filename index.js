2be8c3ec573b21395111b8e8ce2f63ed1c52d740
Laurent <llaumwema733@gmail.com>
2025-12-03 23:40:16 +1030

    add all this file

---
+++ index.js
@@ -0,0 +1,19 @@
+
+   
+   
+const buttons = document.querySelectorAll('button')
+buttons.forEach(btn => btn.addEventListener('click', ()=>{
+    
+const add = parseInt(btn.getAttribute('data-plus'), 10) || 1
+    
+//   console.log(add)
+  const container = btn.closest('#score-board').querySelector('.score-area .score-value');
+  
+  if(!container)return
+//   console.log(container.textContent)
+  
+  container.textContent = (parseInt(container.textContent, 10) || 1) + add
+   
+}))
+
+
